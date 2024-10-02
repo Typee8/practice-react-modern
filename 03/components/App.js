2acca10e695576d13fd80718d665bede8d5a2232
Typee8 <wojciechjasiak1324@gmail.com>
2024-10-02 17:10:10 +0200

    Finish 03

--- 03/components/App.js
+++ 03/components/App.js
@@ -1,15 +1,19 @@
 // ./src/components/App.js
-import React from 'react';
 import Box from './Box';
 import Div from './Div';
+import { TextContext } from '../context';
 
-const App = () => {
+function App() {
+    const { Provider } = TextContext;
+
     return (
         <section>
             <Box />
-            <Div />
+            <Provider value="sibling">
+                <Div />
+            </Provider>
         </section>
     );
-};
+}
 
 export default App;
